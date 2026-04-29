8fed5eb49b24fd84291a6b0db3dad461e9ed4e7e
Raffaele Sorrentino <sorrentinoraffaele05@email.com>
2026-04-29 09:50:06 +0200

    problema con la classe enum

--- src/main/java/Ruolo.java
+++ src/main/java/Ruolo.java
@@ -1,4 +1,5 @@
 
 public enum Ruolo {
-
+	ADMIN,
+	UTENTE
 }
--- src/main/java/Utente.java
+++ src/main/java/Utente.java
@@ -1,4 +1,67 @@
 
 public class Utente {
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private String indirizzo;
+	private Ruolo r;
+	
+	public Utente() {	}
+	
+	public int getId() {
+		return id; 
+		}
+    public void setId(int id) { 
+    	this.id = id; 
+    	}
 
+    
+    
+    public String getUsername() { 
+    	return username; 
+    	}
+    public void setUsername(String username) {
+    	this.username = username; 
+    	}
+
+    
+    
+    public String getEmail() {
+    	return email; 
+    	}
+    public void setEmail(String email) {
+    	this.email = email; 
+    	}
+
+    
+    
+    public String getPassword() { 
+    	return password; 
+    	}
+    public void setPassword(String password) {
+    	this.password = password; 
+    	}
+
+    
+    
+    public String getIndirizzo() {
+    	return indirizzo; 
+    	}
+    public void setIndirizzo(String indirizzo) {
+    	this.indirizzo = indirizzo; 
+    	}
+
+    
+    public Ruolo getRuolo() {
+    	return r;
+    }
+    public void setRuolo(Ruolo r) {
+    	this.r = r;
+    }
+    
+    
+    public boolean isAdmin() {
+    	return Ruolo.ADMIN.equals(r);
+    }
 }
